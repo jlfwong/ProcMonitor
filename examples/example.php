d381d4ad826c68ddaee6c4f1c4f7d9dec5d188e8
Jamie Wong <jamie.lf.wong@gmail.com>
2009-11-24 14:55:38 -0500

    Project Start!

---
+++ examples/example.php
@@ -0,0 +1,11 @@
+<?
+include("ProcMonitor.class.php");
+
+list($fileId,$procId) = ProcMonitor::Start("locate home");
+
+while (ProcMonitor::Running($procId) || ProcMonitor::OutputWaiting($fileId)) {
+		print ProcMonitor::GetOutput($fileId);
+}
+
+ProcMonitor::Close($fileId,$procId);
+?>
