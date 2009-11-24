d2fcdba089208585650cfd41de208f81238623c5
Jamie Wong <jamie.lf.wong@gmail.com>
2009-11-24 15:06:53 -0500

    Indentation fix

--- examples/example.php
+++ examples/example.php
@@ -4,7 +4,7 @@ include("ProcMonitor.class.php");
 list($fileId,$procId) = ProcMonitor::Start("locate home");
 
 while (ProcMonitor::Running($procId) || ProcMonitor::OutputWaiting($fileId)) {
-		print ProcMonitor::GetOutput($fileId);
+	print ProcMonitor::GetOutput($fileId);
 }
 
 ProcMonitor::Close($fileId,$procId);
